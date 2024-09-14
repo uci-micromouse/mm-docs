94e88966421b5c12e0dcb8b1463a0cf71f2c5fe5
Lucas Zhong <zhonglucas@gmail.com>
2024-09-13 22:44:44 -0700

    Add intro page to Documentation page, redirect Get Started button to that

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/documentation/intro">
             Get Started
           </Link>
         </div>
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
